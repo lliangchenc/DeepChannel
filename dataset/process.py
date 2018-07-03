cab4a4c2f1914c522a2782b3e4bc04d9d84419c4
sjx <shijx12@163.com>
2018-07-03 20:29:20 +0800

    Modify process.py to split dataset based on cnndaily_url_splits, and obtain vocab only from train set.

--- dataset/process.py
+++ dataset/process.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 import spacy
 import random
+import hashlib
 from tqdm import tqdm
 from collections import Counter
 #from IPython import embed
@@ -14,14 +15,36 @@ random.seed(666)
 
 pattern_of_num = re.compile(r'[0-9]+')
 nlp = None
-def doc_dealer(d):
+def process_document(d, sentence_len_limit):
+    '''
+    functions:
+        - lowercase
+        - tokenize
+        - replace numbers with 'zero'
+        - remove sentences ending with ':' or '--'
+        - remove sentences whose length <= sentence_len_limit
+    '''
     global nlp
     if nlp is None:
         nlp = spacy.load('en')
-    words = [['zero' if pattern_of_num.match(w.text) else w.text for w in s if w.text.strip()] \
-            for s in nlp(d.lower()).sents if s.text.strip()]
-    return words
-    # return a list of sentences, sentence is list of words
+    d = d.lower()
+    tokenize_d = nlp(d)
+    results = []
+    for s in tokenize_d.sents:
+        if not s.text.strip():
+            continue
+        sentence = []
+        for w in s:
+            if not w.text.strip():
+                continue
+            if pattern_of_num.match(w.text):
+                sentence.append('zero')
+            else:
+                sentence.append(w.text)
+        if sentence[-1]==':' or sentence[-1]=='--' or len(sentence)<=sentence_len_limit:
+            continue
+        results.append(sentence)
+    return results 
 
 
 
@@ -34,41 +57,49 @@ def doc_dealer(d):
 #            data[i].append(list(map(sent_dealer, [f[0], ' '.join(f[1:])])))
 #    return data
 
+def hashhex(s):
+    """Returns a heximal formated SHA1 hash of the input string."""
+    h = hashlib.sha1()
+    h.update(s)
+    return h.hexdigest()
 
 
 def read_cnn_dailymail(data_type, data_dir):
+    def key2File(data_dir, key2file):
+        for k in os.listdir(data_dir):
+            f = os.path.join(data_dir, k)
+            k = k[:-6] # exclude suffix '.story'
+            key2file[k] = f
+
     data = [[], [], []]
     length = [[], [], []] # length of each sentence, whose index is corresponding to data
+    key2file = {} # key: hashkey, excluding the suffix '.story'; file: absolute file path.
     if data_type == 'cnn+dailymail':
         data_dir = data_dir.split(';')
         assert len(data_dir)==2
-        fs = [os.path.join(data_dir[0], _) for _ in os.listdir(data_dir[0])] + [os.path.join(data_dir[1], _) for _ in os.listdir(data_dir[1])]
-    elif data_type == 'cnn' or data_type == 'daily':
-        print(data_dir)
-        assert ';' not in data_dir
-        fs = [os.path.join(data_dir, _) for _ in os.listdir(data_dir)]
-    random.shuffle(fs)
-    split_idx = [0, int(len(fs)*0.8), int(len(fs)*0.9), len(fs)]  # split index for train:valid:test, ratio is 8:1:1
-    for i in range(3):
-        for f in tqdm(fs[split_idx[i]: split_idx[i+1]]):
-            parts = open(f).read().split('@highlight')
-            docu = doc_dealer(parts[0]) 
-            summ = doc_dealer('.'.join(parts[1:]) + '.')
-            docu_ = []
-            summ_ = []
-            for sent in docu:
-                if(len(sent) > 5 and not(sent[-1] == ":" or sent[-1] == "--")):
-                    docu_.append(sent)
-            for sent in summ:
-                if(len(sent) > 3 and not(sent[-1] == ":" or sent[-1] == "--")):
-                    summ_.append(sent)
-            if len(docu_)==0 or len(summ_)==0:
+        for i in range(2):
+            key2File(data_dir[i], key2file) # update key2file
+        prefix = './cnndaily_url_splits/all_'
+    elif data_type == 'cnn':
+        key2File(data_dir, key2file)
+        prefix = './cnndaily_url_splits/cnn_'
+    elif data_type == 'daily':
+        key2File(data_dir, key2file)
+        prefix = './cnndaily_url_splits/dailymail_'
+    for i, split in enumerate(['train', 'val', 'test']):
+        url_file = prefix + split + '.txt'
+        for line in tqdm(open(url_file).readlines()):
+            k = hashhex(line.strip().encode())
+            f = key2file[k] # file path
+            parts = open(f, encoding='latin1').read().split('@highlight')
+            docu = process_document(parts[0], 5)
+            summ = process_document('.'.join(parts[1:]) + '.', 3)
+            if len(docu)==0 or len(summ)==0:
                 continue
-            docu_len = [len(s) for s in docu_]
-            summ_len = [len(s) for s in summ_]
-            data[i].append([docu_, summ_])
+            docu_len = [len(s) for s in docu]
+            summ_len = [len(s) for s in summ]
+            data[i].append([docu, summ])
             length[i].append([docu_len, summ_len])
-            # return data, length
     return data, length
 
 
@@ -123,28 +154,28 @@ def main():
             'duc2007': read_duc2007,
             }
     parser = argparse.ArgumentParser()
-    parser.add_argument('--glove', default='/data/sjx/glove.840B.300d.py36.pkl', help='pickle file of glove300d')
+    parser.add_argument('--glove', default='/data/sjx/glove.6B.100d.py36.pkl', help='pickle file of glove')
     parser.add_argument('--data', default='cnn+dailymail', choices=datasets.keys())
     parser.add_argument('--data-dir', default='/data/share/cnn_stories/stories;/data/share/dailymail_stories/stories', help='If data=cnn+dailimail, then data-dir must contain two paths for cnn and dailymail seperated by ;.')
     parser.add_argument('--save-path', required=True)
-    parser.add_argument('--max-word-num', type=int, default=150000)
+    parser.add_argument('--max-word-num', type=int, default=50000)
     args = parser.parse_args()
 
     print('Loading glove......')
     glove = pickle.load(open(args.glove, 'rb'))
     word_dim = len(glove['the'])
+    print('Word dim = %d' % word_dim)
 
     print('Reading data......')
     data, length = datasets[args.data](args.data, args.data_dir)
     print('train/valid/test: %d/%d/%d' % tuple([len(_) for _ in data]))
-    print('Count word frequency......')
+    print('Count word frequency only from train set......')
     wtof = {}
-    for i in range(len(data)): # train/valid/test
-        for j in range(len(data[i])): # j-th sample
-            for k in range(2): # 0: content, 1: summary
-                for l in range(len(data[i][j][k])): # l-th sentence
-                    for word in data[i][j][k][l]:
-                        wtof[word] = wtof.get(word, 0) + 1
+    for j in range(len(data[0])): # j-th sample of train set
+        for k in range(2): # 0: content, 1: summary
+            for l in range(len(data[i][j][k])): # l-th sentence
+                for word in data[i][j][k][l]:
+                    wtof[word] = wtof.get(word, 0) + 1
     wtof = Counter(wtof).most_common(args.max_word_num)
     needed_words = { w[0]: w[1] for w in wtof }
     print('Preserve word num: %d. Examples: %s %s' % (len(needed_words), wtof[0][0], wtof[1][0]))
